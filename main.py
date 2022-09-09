9deb60fbfd171f0f050a5a387da41486ab3aa0f6
yperetz <yperetz26@gmail.com>
2022-09-09 07:03:21 +0300

    modify to webhooks

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import os
 import requests
 import threading
 import urllib.request
+from flask import Flask, render_template, request
+import telegram
 
-load_dotenv()
+app = Flask(__name__)
+
+# load_dotenv()
+
+
 APIKEY = os.environ.get('APIKEY')
 URL = f"https://api.telegram.org/bot{APIKEY}/"
 
@@ -14,45 +20,74 @@ session = {}
 IMAGE_PATH = "image.png"
 
 
+@app.route("/", methods=['GET', 'POST'])
+def webhook():
+    bot = telegram.Bot(token=os.environ["APIKEY"])
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.effective_chat.id
+        text = update.message.text
+        first_name = update.effective_chat.first_name
+        message = update.message
+        if message is not None:
+            if message.photo is not None:
+                # message.reply_photo()
+                watermark(message)
+            else:
+                bot.sendMessage(chat_id=chat_id, text=f"not a photo",
+                                reply_to_message_id=message.message_id)
+        bot.sendMessage(chat_id=chat_id, text=f"an error occurred")
+
+        # Reply with the same message
+        return 'ok'
+    return 'error'
+
+
 def index():
-    response = requests.get(URL + 'getUpdates')
-    updates = response.json()
-    # print(get_updates)
-    result = updates['result']
-    # print(result)
-    if (len(result) > 0):
-        if 'last_update_id' not in session:
-            # get last update
-            last_update_num = len(result) - 1
-            update_id = result[last_update_num]['update_id']
-            session['last_update_id'] = update_id
-        else:
-            for update in result:
-                # respond to all new messages
-                update_id = update['update_id']
-                if (update_id > session['last_update_id']):
-                    print(update)
-                    message = update['message']
-                    chat = message['chat']
-                    if 'photo' not in message:
-                        answer = "not a photo"
-                        response = requests.get(
-                            f"{URL}sendMessage?chat_id={chat['id']}&text={answer}"
-                            f"&reply_to_message_id={message['message_id']}"
-                            f"&parse_mode=HTML")
-                    else:
-                        watermark(message)
-                    print(response.json())
-                    session['last_update_id'] = update_id
-    return 0
+    return webhook()
+
+
+# def index():
+#     response = requests.get(URL + 'getUpdates')
+#     updates = response.json()
+#     # print(get_updates)
+#     result = updates['result']
+#     # print(result)
+#     if (len(result) > 0):
+#         if 'last_update_id' not in session:
+#             # get last update
+#             last_update_num = len(result) - 1
+#             update_id = result[last_update_num]['update_id']
+#             session['last_update_id'] = update_id
+#         else:
+#             for update in result:
+#                 # respond to all new messages
+#                 update_id = update['update_id']
+#                 if (update_id > session['last_update_id']):
+#                     print(update)
+#                     message = update['message']
+#                     chat = message['chat']
+#                     if 'photo' not in message:
+#                         answer = "not a photo"
+#                         response = requests.get(
+#                             f"{URL}sendMessage?chat_id={chat['id']}&text={answer}"
+#                             f"&reply_to_message_id={message['message_id']}"
+#                             f"&parse_mode=HTML")
+#                     else:
+#                         watermark(message)
+#                     print(response.json())
+#                     session['last_update_id'] = update_id
+#     return 0
 
 
 def watermark(message):
-    file_id = message['photo'][len(message['photo']) - 1]['file_id']
+    file_id = message.photo[len(message.photo) - 1].file_id
+    # file_id = message['photo'][len(message['photo']) - 1]['file_id']
     getFile = requests.get(f'{URL}getFile?file_id={file_id}').json()
     file_path = getFile['result']['file_path']
     photo_url = f"https://api.telegram.org/file/bot{APIKEY}/{file_path}"
-    data = {'chat_id': message['chat']['id']}
+    data = {'chat_id': message.chat.id}
+    # data = {'chat_id': message['chat']['id']}
 
     # downloads the image frorm telegram
     urllib.request.urlretrieve(photo_url, IMAGE_PATH)
@@ -62,7 +97,7 @@ def watermark(message):
         answer = "Failed processing image"
         response = requests.get(
             f"{URL}sendMessage?chat_id={data['chat_id']}&text={answer}"
-            f"&reply_to_message_id={message['message_id']}"
+            f"&reply_to_message_id={message.message_id}"
             f"&parse_mode=HTML")
         return
 
@@ -71,7 +106,7 @@ def watermark(message):
         response = requests.post(
             f"https://api.telegram.org/bot{APIKEY}/sendPhoto?"
             f"chat_id={data['chat_id']}&"
-            f"reply_to_message_id={message['message_id']}",
+            f"reply_to_message_id={message.message_id}",
             files=files)
 
         # response = requests.get(
@@ -79,16 +114,15 @@ def watermark(message):
         #     f"&reply_to_message_id={message['message_id']}"
         #     f"&parse_mode=HTML")
 
-
 # answer = f"You have just sent a photo: = {photo_url}"
 
 
-def run_update():
-    threading.Timer(5.0, run_update).start()
-    index()
+# def run_update():
+#     threading.Timer(5.0, run_update).start()
+#     index()
 
 
-run_update()
+# run_update()
 
 # class TelegramBot:
 #     def __init__(self):
